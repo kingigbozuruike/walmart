a98ee4ca9c5725d9ee2346ee11e30c5ad5476138
chimodo <110926964+chimodo@users.noreply.github.com>
2025-02-02 20:33:27 -0600

    Add files via upload

---
+++ walmart.py
@@ -0,0 +1,97 @@
+# walmart
+BUDGET = 100.00
+LOGO = f"{"="*60}{'\n\u2732  Walmart | Save money. Live better.\n'}{"="*60}"
+cash = BUDGET
+stock = [
+    {"name": "FOHERE Waffle Maker", "price": 39.99},
+    {"name": "Keurig Coffee Maker", "price": 59.00},
+    {"name": "LEGO Classic Box", "price": 34.97},
+    {"name": "Ozark Trail Sleeping Bag", "price": 24.44},
+    {"name": "8-Cube Storage", "price": 84.00},
+    {"name": "Mainstays Coffee Maker", "price": 19.92},
+    {"name": "Great Value Water (32pk)", "price": 5.98},
+    {"name": "Food Storage Set", "price": 9.97},
+    {"name": "HART Claw Hammer", "price": 8.88},
+    {"name": "BIC Ballpoint Pens (10pk)", "price": 2.47},
+    {"name": "Crayola 24-Color Pencils", "price": 3.97},
+    {"name": "Elmer's Glue Sticks (2pk)", "price": 1.97},
+    {"name": "Great Value Paper Towels", "price": 4.98}
+]
+
+cart = [
+    #{"index":1, "name":"Waffle Maker", "price":56.99, "quantity": 2}
+]
+
+def shop():
+    # display items
+    while True:
+        display_items()
+        choice = input("Enter item index to select item\nEnter 'c' to view cart or 'p' to proceed to checkout: ")
+
+        # check for item adding or opening cart
+        if choice.lower() == "c":
+            view_cart()
+        elif choice.lower() == "p":
+            if checkout(): # returns true if checkout successful
+                break
+        else:
+            while True:
+                try:
+                    index = int(choice)
+                    if not index in range(13):
+                        index = int(input("please enter a number between 0 and 12!" ))
+                    else:
+                        add_item(index)
+                        break
+                except:
+                    print("please enter a valid index, c to view cart ot p to check out")
+
+
+
+        # allow user to select item, quantity
+        # notify the user if cart add unsuccessful
+    
+
+# Adding item to the cart
+def add_item(index):
+    
+    # calculate tax
+    x = calculate_tax(stock[index[]])
+
+    
+    # check if addition with tax out of budget 
+    # add to cart and subtract amount *with tax* from cash
+    # cart dict looks like {"index":1, "name":"Waffle Maker", "price":56.99, "quantity": 2}
+    # increment quantity if item is already in the array
+    pass
+
+#calculating tax
+def calculate_tax(price):
+    return price * (10.44 / 100)    
+
+def display_items():
+    global stock
+    print(LOGO)
+    for i, item in enumerate(stock):
+        print(f'{i}) {item["name"]:<30}: ${item["price"]}')
+    
+    #print("Enter the item index to add to cart: ")
+def checkout():
+    # displays the cart one more time and asks the user to confirm the purchase. Return false if they say no and true if they say yes
+    return False
+
+def view_cart():
+    # iterate through the array and print the cart items in a table format
+    pass
+
+def display_receipt():
+    # show items with tax added. for each item multiply the price by the count 
+    # if an item is $10 and you bought 3
+
+    # item name x 3 : $30
+    
+    # tax : $30 @ 10.44% : 3.13
+    # balance to pay : $33.13
+    pass
+
+shop()
